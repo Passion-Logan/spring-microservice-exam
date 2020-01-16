5c4da8e10a0a4eb12b4ffae5d132d170e2d3466c
passion-logan <wangdadamailbox@163.com>
2020-01-16 18:41:53 +0800

    添加服务名常量

---
+++ common/common-core/src/main/java/com/github/tangyi/common/core/constant/MqConstant.java
@@ -0,0 +1,19 @@
+package com.github.tangyi.common.core.constant;
+
+/**
+ * 应用模块名称
+ * <p>
+ * 代码描述
+ * <p>
+ *
+ * @author WQL
+ * @since 2020年1月16日 0016 18:40
+ */
+public class MqConstant {
+
+    /**
+     * 提交考试
+     */
+    public static final String SUBMIT_EXAMINATION_QUEUE = "submit_examination_queue";
+
+}
---
+++ common/common-core/src/main/java/com/github/tangyi/common/core/constant/ServiceConstant.java
@@ -0,0 +1,34 @@
+package com.github.tangyi.common.core.constant;
+
+/**
+ * 服务名常量
+ * <p>
+ * 代码描述
+ * <p>
+ *
+ * @author WQL
+ * @since 2020年1月16日 0016 18:40
+ */
+public class ServiceConstant {
+
+    /**
+     * 用户服务名称
+     */
+    public static final String USER_SERVICE = "user-service";
+
+    /**
+     * 考试服务名称
+     */
+    public static final String EXAM_SERVICE = "exam-service";
+
+    /**
+     * 授权服务名称
+     */
+    public static final String AUTH_SERVICE = "auth-service";
+
+    /**
+     * 消息中心服务名称
+     */
+    public static final String MSC_SERVICE = "msc-service";
+
+}
