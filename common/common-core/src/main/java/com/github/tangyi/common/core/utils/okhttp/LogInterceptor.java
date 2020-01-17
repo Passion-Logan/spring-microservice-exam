45c711f66a79013e3875fe60f781b188edc10ffa
passion-logan <wangdadamailbox@163.com>
2020-01-17 12:45:18 +0800

    日志拦截器

---
+++ common/common-core/src/main/java/com/github/tangyi/common/core/utils/okhttp/LogInterceptor.java
@@ -0,0 +1,24 @@
+package com.github.tangyi.common.core.utils.okhttp;
+
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * 日志拦截器
+ * <p>
+ * 代码描述
+ * <p>
+ *
+ * @author WQL
+ * @since 2020年1月17日 0017 12:44
+ */
+public class LogInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
+}
