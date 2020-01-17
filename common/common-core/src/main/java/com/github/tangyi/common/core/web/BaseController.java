bbd8f9cff1544d937c6c0fa5eeb151fdbabbcb56
passion-logan <wangdadamailbox@163.com>
2020-01-17 16:20:53 +0800

    添加基础控制器

---
+++ common/common-core/src/main/java/com/github/tangyi/common/core/web/BaseController.java
@@ -0,0 +1,13 @@
+package com.github.tangyi.common.core.web;
+
+/**
+ * 基础Controller
+ * <p>
+ * 代码描述
+ * <p>
+ *
+ * @author WQL
+ * @since 2020年1月17日 0017 16:20
+ */
+public abstract class BaseController {
+}
